854177176636370b29349809dbecc7123351225c
Rafael Acioly <aciolyr@gmail.com>
2024-09-03 22:28:00 -0300

    fix policy document version

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const policyResponse = (effect, resource) => {
     return {
         principalId: 'user',
         policyDocument: {
-            Version: '2024-09-03',
+            Version: '2012-10-17',
             Statement: [
                 {
                     Action: 'execute-api:Invoke',
